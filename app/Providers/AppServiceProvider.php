04937be09e11b0b1b6d2326407fa40a3342e9073
kcaballero553378-cyber <k.caballero.553378@umindanao.edu.ph>
2026-05-17 03:24:27 +0800

    Updated project files

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,23 @@
 <?php
-
-namespace App\Providers;
-
-use Illuminate\Support\ServiceProvider;
-
-class AppServiceProvider extends ServiceProvider
+    namespace App\Providers;
+    use Illuminate\Support\ServiceProvider;
+    use Illuminate\Support\Facades\URL;
+    class AppServiceProvider extends ServiceProvider
+    {
+/**
+* Register any application services.
+*/
+public function register(): void
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+//
+}
+/**
+* Bootstrap any application services.
+*/
+public function boot(): void
+{
+if (env('APP_ENV') === 'production') {
+URL::forceScheme('https');
+}
+}
 }
